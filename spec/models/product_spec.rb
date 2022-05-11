edc4f2d782472b0b4593fafed7c407d9071b68db
ryanallenk <ryanallenk@gmail.com>
2022-05-10 20:15:01 -0400

    add product model tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+it "is valid" do
+  @product = Product.new
+  @category = Category.new
+  @category.name = "Test"
+  @product.name = "Test"
+  @product.price_cents = 11822
+  @product.quantity = 5
+  @product.category = @category
+  expect(@product.valid?).to be true
+end
+
+it 'should have a name' do
+  @product = Product.new(name: nil)
+  @product.valid?
+  expect(@product.errors.full_messages).to include("Name can't be blank")
+end
+
+it 'should have a price' do
+  @product = Product.new(price_cents: nil)
+  @product.valid?
+  expect(@product.errors.full_messages).to include("Price can't be blank")
+end
+
+it 'should have a quantity' do
+  @product = Product.new(quantity: nil)
+  @product.valid?
+  expect(@product.errors.full_messages).to include("Quantity can't be blank")
+end
+
+it 'should have a category' do
+  @product = Product.new(category: nil)
+  @product.valid?
+  expect(@product.errors.full_messages).to include("Category can't be blank")
+end
+
+
+end
