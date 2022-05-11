4c4133061afb66ebcf473fbb40c1bd57eda01faa
ryanallenk <ryanallenk@gmail.com>
2022-05-11 16:22:21 -0400

    add home spec test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,17 @@
+describe('jungle app', () => {
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000')
+  // }) 
+  it ('visits the homepage', () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+});
